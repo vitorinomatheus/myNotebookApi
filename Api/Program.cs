cfd94c5f8b3beaa63b54435d61d79e4d8c421e66
vitorinomatheus <matheusvs122@gmail.com>
2022-12-16 00:29:56 -0300

    develop hash password class

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,13 @@
 using Application.Services;
 using Domain.Interfaces;
+using Domain.Interfaces.UtilsInterfaces;
 using Domain.Interfaces.Services;
 using Infra.Repositories;
 using Infra.Data;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Dtos;
+using Infra.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +21,10 @@ var config = new MapperConfiguration(cfg => {
 var mapper = config.CreateMapper();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IHashPasswords, HashPasswords>();
+builder.Services.AddScoped<PasswordSaltRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddSingleton(mapper);
 
